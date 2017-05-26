6561299c4b1803a37adf86dd887aa78c71edc647
Ruturaj Powar <ruturajp@cdac.in>
2017-05-26 18:13:22 +0530

    second commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!');
+requirejs.config({
+    baseUrl: 'scripts/modules',
+    path: {
+        //The libraries we use
+        jquery: [
+            'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+            //if cdn location fails load from this location
+            'lib/jquery-3.2.1.min'
+        ],
+        underscore: 'lib/underscore',
+        bootstrap: 'lib/bootstrap.min'
+    }
+});
---
+++ app/scripts/modules/page/home.js
@@ -0,0 +1,5 @@
+console.log("in home.js");
+define(['jquery'], function ($) {
+    console.log("in jquery");
+    $("#foo").text("change by jquery with help of require js");
+});
